c21de5383450fee4535f281b30505fe598f462a3
Dhruv-Git21 <146096570+Dhruv-Git21@users.noreply.github.com>
2025-11-24 12:34:12 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/websiteDM/',
+  base: '/websiteDM/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
@@ -13,3 +13,4 @@ export default defineConfig({
     },
   },
 });
+
